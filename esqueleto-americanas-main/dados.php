9a08d2b796551750c08bb8bd69cf843be329c132
Vinicius Marin <125417990+ViniMarin@users.noreply.github.com>
2023-10-26 22:21:39 -0300

    Add files via upload

---
+++ esqueleto-americanas-main/dados.php
@@ -0,0 +1,31 @@
+<?php
+
+    $produtos[1] = array("id"=>1,
+        "nome"=>"Ecolapis de Cor Grip 24 Cores",
+        "valor"=>"34,00",
+        "imagem"=>"01.webp");
+    $produtos[2] = array("id"=>2,
+        "nome"=>"Marca Texto Textliner Pastel",
+        "valor"=>"65,00",
+        "imagem"=>"02.webp");
+    $produtos[3] = array("id"=>3,
+        "nome"=>"Estojo com 24 Cores de Pastel Oleoso",
+        "valor"=>"124,00",
+        "imagem"=>"03.webp");
+    $produtos[4] = array("id"=>4,
+        "nome"=>"Pincel Marcador Preto",
+        "valor"=>"8,00",
+        "imagem"=>"04.webp");
+    $produtos[5] = array("id"=>5,
+        "nome"=>"Caneta Esferografica Trilux Ponta Média 1.0 Office",
+        "valor"=>"6,00",
+        "imagem"=>"05.webp");
+    $produtos[6] = array("id"=>6,
+        "nome"=>"Aspirador Água e Pó Electrolux Awd01 Hidrolux 1250w - 220v",
+        "valor"=>"279,90",
+        "imagem"=>"1237561210P.webp");
+    $produtos[7] = array("id"=>7,
+        "nome"=>"Caixa de Som JBL Go 3, Bluetooth, Azul",
+        "valor"=>"290,90",
+        "imagem"=>"3032309897P.webp");                                        
+        
---
+++ esqueleto-americanas-main/produto.php
@@ -0,0 +1,24 @@
+<?php
+
+include "header.php";
+include "dados.php";
+$id = $_GET["id"]; //esse id venda URL produto.php?id=2
+?>
+
+<main>
+    <div class="grid-2">
+        <div class="coluna">
+            <img src="imagens/<?=$produtos[$id]["imagem"] ?>" alt="<?=$produtos[$id]["nome"]?>">
+        </div>
+        <div class="coluna">
+            <h2><?=$produtos[$id]["nome"]?></h2>
+            <p>R$ <?=$produtos[$id]["valor"]?></p>
+        </div>
+    </div>
+</main>
+
+<?php
+
+include "footer.php";
+
+?>
